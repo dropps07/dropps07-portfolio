d4ad5bbfcae0cf06f1ff2ddd796b4f4d89c919b9
Ajey Awasthi <115537579+dropps07@users.noreply.github.com>
2025-03-20 23:38:22 +0530

    Fixed img not showing error

--- src/constants/worksData.tsx
+++ src/constants/worksData.tsx
@@ -3,14 +3,14 @@ export const WorksData = [
     {
         "heading" : "tixets",
         "description" : "A Decentralized Event Ticketing platform providing a secure, transparent and a reliable solution.",
-        "src":"/tixets",
+        "src":"/tixets.png",
         "link": "https:github.com/dropps07/tixets",
         "deploy": "https://tixets.vercel.app/"
     },
     {
         "heading" : "chatApp",
         "description": "A chat app using websockets for real time chatting with nextjs as a frontend and Go in the backend.",
-        "src":"/chatApp",
+        "src":"/chatApp.png",
         "link" : "https://github.com/dropps07/chatapp-nextjs-go",
         "deploy": "server down 404"
     }
