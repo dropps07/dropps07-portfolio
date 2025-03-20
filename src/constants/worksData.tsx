e589ee4e97d4cd008fba012901d8e19f7e99d7e9
dropps07 <pungxbazz@gmail.com>
2025-03-20 22:12:59 +0530

    fixed errors

--- src/constants/worksData.tsx
+++ src/constants/worksData.tsx
@@ -1,15 +1,17 @@
+import tixets from "../../../public/tixets.png"
+import chatApp from "../../../public/chatApp.png"
 export const WorksData = [
     {
         "heading" : "tixets",
         "description" : "A Decentralized Event Ticketing platform providing a secure, transparent and a reliable solution.",
-        "src":"/tixets.png",
+        "src":tixets,
         "link": "https:github.com/dropps07/tixets",
         "deploy": "https://tixets.vercel.app/"
     },
     {
         "heading" : "chatApp",
         "description": "A chat app using websockets for real time chatting with nextjs as a frontend and Go in the backend.",
-        "src":"/chatApp.png",
+        "src":chatApp,
         "link" : "https://github.com/dropps07/chatapp-nextjs-go",
         "deploy": "server down 404"
     }
