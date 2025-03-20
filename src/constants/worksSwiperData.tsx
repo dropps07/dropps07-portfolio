9cf496e5911e0ca0453fb6a67fc71a0c1546a19c
dropps07 <pungxbazz@gmail.com>
2025-03-20 23:24:16 +0530

    updating

--- src/constants/worksSwiperData.tsx
+++ src/constants/worksSwiperData.tsx
@@ -1,7 +1,7 @@
 export const WorksSwiperData = [
     {
         "heading" : " RESTful Apis",
-        "description" : "build Restful apis in Go as a learning lesson to get started in Golang.",
+        "description" : "built Restful apis in Go as a learning lesson to get started in Golang.",
         "link":"https://github.com/dropps07/RESTful-Apis-using-Go"
     },
     {
@@ -11,7 +11,7 @@ export const WorksSwiperData = [
     },
     {
         "heading" : " RESTful Apis",
-        "description" : "build Restful apis in Go as a learning lesson to get started in Golang.",
+        "description" : "built Restful apis in Go as a learning lesson to get started in Golang.",
         "link":"https://github.com/dropps07/RESTful-Apis-using-Go"
     },
     {
